30ac724ca5fc6f6ba6fb6a24377b125a3e4701d3
huxhu <45440969+huxhu@users.noreply.github.com>
2018-12-14 21:29:18 -0500

    Add files via upload

--- home_view.php
+++ home_view.php
@@ -47,3 +47,23 @@
     </table>
 </main>
 <?php include 'view/footer.php'; ?>
+
+<html lang="en-US">
+<body>
+
+<p>Translate this page:</p>
+
+<div id="google_translate_element"></div>
+
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+<p>You can translate the content of this page by selecting a language in the select box.</p>
+
+</body>
+</html>
